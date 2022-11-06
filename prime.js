bb32f983f514d07c0f095f8160367e911bd00b2f
AnshKathpal <storage.2.3107@gmail.com>
2022-11-06 22:03:45 +0530

    created code file

---
+++ prime.js
@@ -0,0 +1,15 @@
+function primeNum(n){  
+let count = 0;
+    for(i=0;i<=n;i++){
+      if(n%i==0){
+        count++;
+     }
+      }
+      if(count=2){
+      console.log("Number is prime")
+      }else {
+      console.log("Number is not prime")
+    }
+     }
+
+    primeNum(9);
